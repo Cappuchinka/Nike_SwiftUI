9d339a1c2195277158b992e9a6e2fffd426b4201
Cappuchinka <volchenko.coffee@gmail.com>
2025-11-12 22:50:50 +0300

    Initial Commit

---
+++ NikeProjectSwiftUI/NikeProjectSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  NikeProjectSwiftUIApp.swift
+//  NikeProjectSwiftUI
+//
+//  Created by volchenkop on 12.11.2025.
+//
+
+import SwiftUI
+
+@main
+struct NikeProjectSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
