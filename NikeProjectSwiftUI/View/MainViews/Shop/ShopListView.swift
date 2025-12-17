efa9ebb73582a4f04cfa257577340f010de6c955
Cappuchinka <volchenko.coffee@gmail.com>
2025-12-18 00:43:39 +0300

    Add filters for shop list

--- NikeProjectSwiftUI/View/MainViews/Shop/ShopListView.swift
+++ NikeProjectSwiftUI/View/MainViews/Shop/ShopListView.swift
@@ -8,13 +8,18 @@
 import SwiftUI
 
 struct ShopListView: View {
+    @State private var selectedTab = 0
     @StateObject private var viewModel = ProductViewModel()
     private let gridLayout = [
         GridItem(.adaptive(minimum: 170))
     ]
 
+    let categoriesTabs = ["All", "Hoodies & Pullovers", "Tops & T-Shirts", "Pants", "Shoes", "Accessories & Equipment"]
+
     var body: some View {
         VStack {
+            ScrollCategoryTabView(selectedTab: $selectedTab, categoriesTabs: categoriesTabs)
+
             ScrollView(.vertical) {
                 LazyVGrid(columns: gridLayout, spacing: 5) {
                     ForEach(viewModel.products) { product in
@@ -24,7 +29,10 @@ struct ShopListView: View {
             }
         }
         .onAppear {
-            viewModel.fetchData()
+            viewModel.fetchData(index: selectedTab)
+        }
+        .onChange(of: selectedTab) { oldValue, newValue in
+            viewModel.fetchData(index: newValue)
         }
         .navigationTitle(Text("Shop"))
     }
